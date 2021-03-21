072c0df69a595b969fc2f907ce9226b2794d73a0
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2021-03-21 10:19:15 -0400

    feat: intial models

--- project/spending/models.py
+++ project/spending/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField()
+    description = models.CharField(max_length=255)
