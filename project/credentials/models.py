029b0d46dd7b73a4c5592c9e7ca2c1e4351adad9
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2021-08-14 17:00:16 -0400

    feat: add token type to credential model

--- project/credentials/models.py
+++ project/credentials/models.py
@@ -8,14 +8,27 @@ from django_cryptography.fields import encrypt
 models.CharField.register_lookup(models.functions.Length)
 
 class Credential(models.Model):
+    PLAID_CLIENT_ID = 'PLAID_CLIENT_ID'
+    PLAID_CLIENT_SECRET = 'PLAID_CLIENT_SECRET'
+    PLAID_PUBLIC_KEY = 'PLAID_PUBLIC_KEY'
+    PLAID_TOKEN = 'PLAID_TOKEN'
+    TOKEN_TYPES = (
+        (PLAID_CLIENT_ID, 'Plaid Client ID'),
+        (PLAID_CLIENT_SECRET, 'Plaid Client Secret'),
+        (PLAID_PUBLIC_KEY, 'Plaid Public Key'),
+        (PLAID_TOKEN, 'Plaid Token'),
+    )
+
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     token = encrypt(models.CharField(max_length=255))
     name = models.CharField(max_length=120)
+    token_type = models.CharField(max_length=50, choices=TOKEN_TYPES)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         constraints = (
             models.CheckConstraint(check=models.Q(name__length__gt=0), name='length_credential_name'),
+            models.CheckConstraint(check=models.Q(token_type__length__gt=0), name='length_credential_tokentype'),
             models.UniqueConstraint(fields=('user', 'token'), name='unique_credential_token'),
             models.UniqueConstraint(fields=('user', 'name'), name='unique_credential_name'),
         )
--- project/credentials/tests.py
+++ project/credentials/tests.py
@@ -6,12 +6,18 @@ from credentials.models import Credential
 
 class CredentialsTestCase(UserTestCase):
     def test_creation(self):
-        credential = Credential(token='foo', name='bar', user=self.user)
+        credential = Credential(token='foo', name='bar', user=self.user,
+                token_type=Credential.PLAID_CLIENT_ID)
         credential.save()
 
     def test_name_required(self):
         """ Ensure name is required on Categories """
-        credential = Credential(user=self.user)
+        credential = Credential(user=self.user, token='abc', token_type=Credential.PLAID_CLIENT_ID)
+        with self.assertRaises(IntegrityError):
+            credential.save()
+
+    def test_token_type_required(self):
+        credential = Credential(user=self.user, token='abc', name='test')
         with self.assertRaises(IntegrityError):
             credential.save()
 
@@ -22,12 +28,16 @@ class CredentialsTestCase(UserTestCase):
         )
 
         for name, token in tokens:
-            credential, created = Credential.objects.get_or_create(name=name, user=self.user, defaults=dict(token=token))
+            credential, created = Credential.objects.get_or_create(
+                    name=name,
+                    user=self.user,
+                    defaults=dict(token=token, token_type=Credential.PLAID_CLIENT_ID)
+                )
             self.assertIsNotNone(credential.uuid)
             self.assertTrue(created)
 
         for name, token in tokens:
             # Try to re-save existing names for this user
-            credential = Credential(name=name, token='token_new_baz', user=self.user)
+            credential = Credential(name=name, token='token_new_baz', token_type=Credential.PLAID_CLIENT_ID, user=self.user)
             self._assertRaisesAtomic(credential.save, IntegrityError)
 
