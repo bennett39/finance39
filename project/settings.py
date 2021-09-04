d72e52e15a061b7e402e71dede421f2d4404c4cf
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2021-09-04 13:07:55 -0400

    build: add whitenoise static files

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Set Heroku settings
 if not STACK_NAME == 'circleci':
